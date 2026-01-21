900c9a204e68b2ec88fa8e76315d9816130924d4
Ignacio Cuñado Barral <ignacunado@gmail.com>
2026-01-21 23:04:29 +0100

    feat: Add custom configs for graphgym

---
+++ src/config/general.py
@@ -0,0 +1,7 @@
+from torch_geometric.graphgym.register import register_config
+
+
+@register_config('extend_general_config')
+def extend_general_config(cfg):
+    cfg.save_model = False
+    cfg.root_dir = ''
---
+++ src/config/gnn.py
@@ -0,0 +1,12 @@
+from torch_geometric.graphgym import register_config
+
+
+@register_config('extend_gnn_config')
+def extend_gnn_config(cfg):
+    # Dimensions
+    cfg.gnn.dim_in = 1
+    cfg.gnn.dim_out = 2
+    cfg.gnn.edge_dim = 4
+
+    cfg.gnn.emlps = False # To use edge updates via MLP
+    cfg.gnn.pna_deg = None
