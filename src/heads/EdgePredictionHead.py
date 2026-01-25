2cad25bfac1ca102052fd947854144b5b1b6b709
Ignacio Cuñado Barral <ignacunado@gmail.com>
2026-01-25 15:54:11 +0100

    fix: Attribute error

--- src/heads/EdgePredictionHead.py
+++ src/heads/EdgePredictionHead.py
@@ -11,9 +11,10 @@ class EdgePredictionHead(nn.Module):
         super(EdgePredictionHead, self).__init__()
 
         self.final_dropout = cfg.gnn.dropout
+        self.dim_inner = cfg.gnn.dim_inner
 
         self.mlp = nn.Sequential(
-            Linear(cfg.gnn.dim_inner * 3, 50),
+            Linear(self.dim_inner * 3, 50),
             nn.ReLU(),
             nn.Dropout(self.final_dropout),
             Linear(50, 25),
@@ -25,6 +26,6 @@ class EdgePredictionHead(nn.Module):
     def forward(self, data):
         x, edge_attr = data.x, data.edge_attr
 
-        x = x[data.edge_index.T].reshape(-1, 2 * self.n_hidden).relu()
+        x = x[data.edge_index.T].reshape(-1, 2 * self.dim_inner).relu()
         x = torch.cat((x, edge_attr.view(-1, edge_attr.shape[1])), 1)
         return self.mlp(x)
