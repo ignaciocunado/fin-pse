e10346a0943a281873a10662593920cc2c30dfd2
Ignacio Cuñado Barral <ignacunado@gmail.com>
2026-03-17 11:44:44 +0100

    fix: Update edge attributes on encodings pretrain

--- src/networks/fin_pse.py
+++ src/networks/fin_pse.py
@@ -11,7 +11,7 @@ class FinPSE(nn.Module):
     def __init__(self, dim_in, dim_out):
         super(FinPSE, self).__init__()
 
-        self.encoder = FinPSEEncoder()
+        self.encoder = FinPSEEncoder(cfg.gnn.dim_in, cfg.gnn.edge_dim, cfg.gnn.dim_inner)
         self.head = head_dict[cfg.gnn.head](cfg.gnn.dim_inner, cfg.gnn.dim_out)  # TODO: Add multiple head outputs
 
     def forward(self, data):
@@ -21,10 +21,10 @@ class FinPSE(nn.Module):
 
 
 class FinPSEEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, dim_in, edge_dim, dim_inner):
         super(FinPSEEncoder, self).__init__()
-        self.node_emb = nn.Linear(cfg.gnn.dim_in, cfg.gnn.dim_inner)
-        self.edge_emb = nn.Linear(cfg.gnn.edge_dim, cfg.gnn.dim_inner)
+        self.node_emb = nn.Linear(dim_in, dim_inner)
+        self.edge_emb = nn.Linear(edge_dim, dim_inner)
 
         self.convs = nn.ModuleList()
         self.batch_norms = nn.ModuleList()
@@ -32,10 +32,10 @@ class FinPSEEncoder(nn.Module):
         for _ in range(cfg.gnn.layers_mp):
             self.convs.append(
                 ResGatedGraphConv(
-                    cfg.gnn.dim_inner, cfg.gnn.dim_inner, edge_dim=cfg.gnn.dim_inner, act=act_dict[cfg.gnn.act]()
+                    dim_inner, dim_inner, edge_dim=dim_inner, act=act_dict[cfg.gnn.act]()
                 )
             )
-            self.batch_norms.append(BatchNorm(cfg.gnn.dim_inner))
+            self.batch_norms.append(BatchNorm(dim_inner))
 
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
@@ -48,4 +48,5 @@ class FinPSEEncoder(nn.Module):
             x = (x + F.relu(self.batch_norms[i](n_new))) / 2
 
         data.x = x
+        data.edge_attr = edge_attr
         return data
